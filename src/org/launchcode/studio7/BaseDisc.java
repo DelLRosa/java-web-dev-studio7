36e11190dd37fdfde48b12ad7c979080dd91f7c5
Delaney <delaney.rosa1@gmail.com>
2023-04-17 07:54:51 -0500

    abstract and interface classes

---
+++ src/org/launchcode/studio7/BaseDisc.java
@@ -0,0 +1,43 @@
+package org.launchcode.studio7;
+
+public class BaseDisc {
+    private double storageCapacity;
+    private String dataType;
+
+    private String dataToBeStored;
+
+    public double getStorageCapacity() {
+        return storageCapacity;
+    }
+
+    public void setStorageCapacity(double storageCapacity) {
+        this.storageCapacity = storageCapacity;
+    }
+
+    public String getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(String dataType) {
+        this.dataType = dataType;
+    }
+
+    public String getDataToBeStored() {
+        return dataToBeStored;
+    }
+
+    public void setDataToBeStored(String dataToBeStored) {
+        this.dataToBeStored = dataToBeStored;
+    }
+
+    public void readData(){
+
+    }
+    public void writeData(){
+
+    }
+    public void reportData(){
+
+    }
+
+}
---
+++ src/org/launchcode/studio7/OpticalDisk.java
@@ -0,0 +1,6 @@
+package org.launchcode.studio7;
+
+public interface OpticalDisk {
+    public void spinDisc();
+    public void storeData();
+}
